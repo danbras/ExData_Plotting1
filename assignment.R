659ee447e0fa0346dea0abf73f1bf8b7a4c54282
danbras <danielsequeira@Daniels-MacBook-Pro.local>
2014-06-06 13:25:55 -0500

    Final commit

--- assignment.R
+++ assignment.R
@@ -1,11 +1,35 @@
 #Read data
-power=read.csv("~/Downloads/household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
-#Convert to Date format
-power$Date=as.Date(power$Date,"%d/%m/%Y")
+power=read.csv("~/Downloads/household_power_consumption.txt",header=TRUE,sep=";",na.strings="?",stringsAsFactors=FALSE)
+#Create a Date/Time Variable and Convert Format
+power$DateTime=strptime(paste(power$Date,power$Time),"%d/%m/%Y %H:%M:%S")
 #Use only two days worth of data
-power=subset(power,power$Date>="2007-02-01"&power$Date<="2007-02-02")
-#Histogram
-png(file="~/Downloads/hist.png")
+power=subset(power,power$DateTime>=as.POSIXlt("2007-02-01 00:00:00")&power$DateTime<as.POSIXlt("2007-02-03 00:00:00"))
+#Plot1
+png(file="~/Downloads/plot1.png")
 hist(power$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)
 ")
+dev.off()
+#Plot2
+png(file="~/Downloads/plot2.png")
+plot(power$DateTime,power$Global_active_power,type="l",main="Global Active Power",ylab="Global Active Power (kilowatts)",xlab="")
+dev.off()
+#Plot3
+png(file="~/Downloads/plot3.png")
+plot(power$DateTime,power$Sub_metering_1,type="n",ylab="Energy sub metering",xlab="")
+lines(power$DateTime,power$Sub_metering_1)
+lines(power$DateTime,power$Sub_metering_2,col="red")
+lines(power$DateTime,power$Sub_metering_3,col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"),cex=.75)
+dev.off()
+#Plot 4
+png(file="~/Downloads/plot4.png")
+par(mfrow=c(2,2))
+plot(power$DateTime,power$Global_active_power,type="l",ylab="Global Active Power",xlab="")
+plot(power$DateTime,power$Voltage,type="l",ylab="Voltage",xlab="datetime")
+plot(power$DateTime,power$Sub_metering_1,type="n",ylab="Energy sub metering",xlab="")
+lines(power$DateTime,power$Sub_metering_1)
+lines(power$DateTime,power$Sub_metering_2,col="red")
+lines(power$DateTime,power$Sub_metering_3,col="blue")
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"),cex=.75)
+plot(power$DateTime,power$Global_reactive_power,type="l",ylab="Global_reactive_power",xlab="datetime")
 dev.off()
